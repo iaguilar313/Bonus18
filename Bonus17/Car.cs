b03c8db79ec33013ab344447144a27019c0e320e
iaguilar313 <iaguilar@umich.edu>
2017-10-09 13:58:46 -0400

    first add

---
+++ Bonus17/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus17
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }        
+        }
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+
+
+        public Car()
+        {
+            make = "";
+            model = "";
+            year = 0;
+            price = 0;
+
+        }
+
+        public Car (string CarMake, string CarModel, int CarYear, double CarPrice)
+        {
+            make = CarMake;
+            model = CarModel;
+            year = CarYear;
+            price = CarPrice;
+
+        }
+
+
+    }
+}
---
+++ Bonus17/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Motors Admin Console");
+            Console.WriteLine("How many cars are you entering");
+
+            int NumberOfCars;
+            NumberOfCars = int.Parse(Console.ReadLine());
+
+            Car[] CarNo = new Car[NumberOfCars];
+
+            for (int i = 0; i < CarNo.Length; i++)
+            {
+                CarNo[i] = new Car();
+            }
+
+            for (int i = 0; i < CarNo.Length; i++)
+            {
+                Console.WriteLine("Enter Car's Make");
+                CarNo[i].Make = Console.ReadLine();
+
+                Console.WriteLine("Enter Car's Model");
+                CarNo[i].Model = Console.ReadLine();
+
+                Console.WriteLine("Enter Car's Year");
+                CarNo[i].Year = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Car's Price");
+                CarNo[i].Price = int.Parse(Console.ReadLine());
+
+
+            }
+
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("Make \tModel \tYear \tPrice"); 
+
+            foreach(Car No in CarNo)
+            {
+                Console.WriteLine($"{No.Make}\t {No.Model}\t {No.Year}\t {No.Price} ");
+            }
+
+                
+            
+
+
+        }
+    }
+}
